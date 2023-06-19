70a6642790c5946256d1912886005fa4d242f876
WilliamFilho <williamfilho20@gmail.com>
2023-06-19 08:29:15 -0300

    - Entity User e H2 Configurado -

--- src/main/java/com/wnet/dscommerce/entities/User.java
+++ src/main/java/com/wnet/dscommerce/entities/User.java
@@ -1,13 +1,24 @@
 package com.wnet.dscommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.time.LocalDate;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private LocalDate birthDate;
     private String password;
-    private String roles;
+
 }
