600bd49f64a22811f182ba59de2987b1482e73ca
WilliamFilho <williamfilho20@gmail.com>
2023-06-19 07:43:44 -0300

    - lombok add + add Entity User -

---
+++ src/main/java/com/wnet/dscommerce/entities/User.java
@@ -0,0 +1,13 @@
+package com.wnet.dscommerce.entities;
+
+import java.time.LocalDate;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+    private String roles;
+}
