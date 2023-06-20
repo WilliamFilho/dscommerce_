21d98443ac482b5f1bee2f5dacd06e56137a8d99
WilliamFilho <williamfilho20@gmail.com>
2023-06-20 14:18:26 -0300

    - add @ControllerAdvice -

---
+++ src/main/java/com/wnet/dscommerce/dto/CustomError.java
@@ -0,0 +1,16 @@
+package com.wnet.dscommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+@AllArgsConstructor
+public class CustomError {
+        private Instant timestamp;
+        private Integer status;
+        private String error;
+        private String path;
+}
