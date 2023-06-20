b366c62f091480a5b04d39fa9e0355cebd7b1d7c
WilliamFilho <williamfilho20@gmail.com>
2023-06-19 23:16:23 -0300

    - DTO product + add Services -

--- src/main/java/com/wnet/dscommerce/controllers/ProductController.java
+++ src/main/java/com/wnet/dscommerce/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.wnet.dscommerce.controllers;
 
+import com.wnet.dscommerce.dto.ProductDTO;
+import com.wnet.dscommerce.entities.Product;
+import com.wnet.dscommerce.repositories.ProductRepository;
+import com.wnet.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping
-    public String teste(){
-        return "William Filho";
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
     }
 }
