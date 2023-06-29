35d3bafbe48b79ed6dc96c86e8c8033a4b485ee8
WilliamFilho <williamfilho20@gmail.com>
2023-06-29 17:28:07 -0300

    - security + dep. + Class Role -

---
+++ src/main/java/com/wnet/dscommerce/entities/Role.java
@@ -0,0 +1,25 @@
+package com.wnet.dscommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+    
+}
