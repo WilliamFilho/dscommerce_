57d58414f4991a0a472d5788b2a1192a50fef3b9
WilliamFilho <williamfilho20@gmail.com>
2023-06-29 18:30:14 -0300

    - security + UserDetailsProjection + properties -

---
+++ src/main/java/com/wnet/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,8 @@
+package com.wnet.dscommerce.projections;
+
+public interface UserDetailsProjection {
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
